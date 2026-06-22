685e1f7821a953628b752c1c5792eb189f55dcaf
Jeff Whiting <jeffw@qualtrics.com>
2026-06-22 11:16:02 -0600

    Initial commit of the modified cougarboard layout

---
+++ cougarboard.user.js
@@ -0,0 +1,78 @@
+// ==UserScript==
+// @name         cougarboard message board first
+// @namespace    http://tampermonkey.net/
+// @version      2026-06-22
+// @description  This redoes the layout and puts the message board first and expands all the more posts.
+// @author       Jeff Whiting
+// @match        https://www.cougarboard.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cougarboard.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const css = `
+    @media (max-width: 3000px) {
+        div#home-grid {
+            display:flex;
+            flex-direction: column;
+            align-items: stretch
+        }
+
+        div#home-main,div#home-rail,section#home-news {
+            display: contents
+        }
+
+        section#home-news>cb-article.featured {
+            order: 2
+        }
+
+        div#home-board {
+            order: 1
+        }
+
+        div#home-ad-board {
+            order: 3
+        }
+
+        section#home-podcasts {
+            order: 4
+        }
+
+        section#home-news>div.news-list {
+            order: 5
+        }
+
+        div#home-ad-news {
+            order: 6
+        }
+
+        section#home-news>div.news-list.news-list-2 {
+            order: 7
+        }
+
+        section#home-news>a.news-more {
+            order: 8
+        }
+
+        div#home-featured {
+            order: 9
+        }
+
+        section#home-promos {
+            order: 10
+        }
+    }`;
+
+    const styleTag = document.createElement('style');
+    styleTag.id = 'injected-layout-override';
+    styleTag.textContent = css;
+    document.head.appendChild(styleTag);
+
+    const data = {
+        success: true,
+        tagId: styleTag.id,
+        message: "CSS injected successfully into the document head."
+    };
+    Array.from(document.getElementsByClassName('board-more')).forEach((e) => e.click());
+})();
---
+++ readme.md
@@ -0,0 +1,4 @@
+This user javascript, re-arranges cougarboards layout and puts the message board content first.
+
+First install TamperMonkey: hhttps://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en&pli=1
+Then navigate to the raw user scripts and choose install: 
